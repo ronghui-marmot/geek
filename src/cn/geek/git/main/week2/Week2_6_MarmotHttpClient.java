a4ecfefba8656356ddbaa38a8c6e2c37b453c8c8
ronghui_19 <ronghui_19@126.com>
2021-05-14 22:56:37 +0800

    commit homework week2-6 firstly

---
+++ src/cn/geek/git/main/week2/Week2_6_MarmotHttpClient.java
@@ -0,0 +1,48 @@
+package cn.geek.git.main.week2;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.hc.client5.http.ClientProtocolException;
+import org.apache.hc.client5.http.classic.methods.*;
+import org.apache.hc.client5.http.impl.classic.*;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.HttpEntity;
+
+public class Week2_6_MarmotHttpClient {
+	
+
+	public static void main(String[] args) throws Exception {
+		try(final CloseableHttpClient httpClient = HttpClients.createDefault()){
+			final String url = "Http://localhost:8801";
+			final HttpGet httpGet = new HttpGet(url);
+			System.out.println("-----------execute request " + httpGet.getMethod() + " " + httpGet.getUri());
+			
+			final HttpClientResponseHandler<String> responseHandle = new HttpClientResponseHandler<String>() {
+				@Override
+				public String handleResponse(ClassicHttpResponse response) throws HttpException, IOException {
+					int status = response.getCode();
+	//				状态码200（OK）到300（重定向）之间的 都为访问成功
+					if(status >= HttpStatus.SC_SUCCESS && status<HttpStatus.SC_REDIRECTION) {
+						final HttpEntity entity = response.getEntity();
+						try {
+							return entity!=null?EntityUtils.toString(entity):null;
+						}catch(ParseException e) {
+							throw new ClientProtocolException(e);
+						}
+					}else {
+						throw new ClientProtocolException("Unexcepted response status:" + status);
+					}
+				}
+			};
+			
+			final String responseBody = httpClient.execute(httpGet,responseHandle);
+			System.out.println(responseBody);
+		}
+	}
+}
