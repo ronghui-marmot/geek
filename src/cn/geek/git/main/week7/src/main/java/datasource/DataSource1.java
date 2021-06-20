7e2e861e3f38b16beb4193fc834031cb2bb78f7b
ronghui_19 <ronghui_19@126.com>
2021-06-20 22:50:15 +0800

    week7 homework secondly

---
+++ src/cn/geek/git/main/week7/src/main/java/datasource/DataSource1.java
@@ -0,0 +1,69 @@
+package datasource;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+@Configuration//注解到spring容器中
+@MapperScan(basePackages = "data1.mapper",sqlSessionFactoryRef = "data1SqlSessionFactory")
+public class DataSource1 {
+
+    /**
+     * 返回data1数据库的数据源
+     * @return
+     */
+    @Bean(name="data1Source")
+    @Primary//主数据源
+    @ConfigurationProperties(prefix = "spring.datasource.data1")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    /**
+     * 返回data1数据库的会话工厂
+     * @param ds
+     * @return
+     * @throws Exception
+     */
+    @Bean(name = "data1SqlSessionFactory")
+    @Primary
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("data1Source") DataSource ds) throws Exception{
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(ds);
+        return bean.getObject();
+    }
+
+    /**
+     * 返回data1数据库的会话模板
+     * @param sessionFactory
+     * @return
+     * @throws Exception
+     */
+    @Bean(name = "data1SqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("data1SqlSessionFactory") SqlSessionFactory sessionFactory) throws  Exception{
+        return  new SqlSessionTemplate(sessionFactory);
+    }
+
+    /**
+     * 返回data1数据库的事务
+     * @param ds
+     * @return
+     */
+    @Bean(name = "data1TransactionManager")
+    @Primary
+    public PlatformTransactionManager  transactionManager(@Qualifier("data1Source") DataSource ds){
+        return new PlatformTransactionManager(ds);
+    }
+}
