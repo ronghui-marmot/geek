4b139e23affd6c02fdabd58186b14f8b4b3570ca
ronghui_19 <ronghui_19@126.com>
2021-08-19 21:22:25 +0800

    happy ending

--- src/cn/geek/git/main/week13/spring-kafka-demo/src/main/java/marmot/KafkaProducer.java
+++ src/cn/geek/git/main/week13/spring-kafka-demo/src/main/java/marmot/KafkaProducer.java
@@ -2,6 +2,8 @@ package marmot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KafkaProducer {
